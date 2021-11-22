56183984e5ebd19471702bbfddafdb9165b7cc87
prateekjain9997 <mdeesha1996@gmail.com>
2021-11-22 11:36:38 +0530

    multer part is added

--- Multer_File_Upload/server.js
+++ Multer_File_Upload/server.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const multer = require('multer');
+const path=require('path')
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "API is working.."
-    })
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, './images')
+    },
+    filename: (req, file, cb) => {
+        cb(null,file.originalname)
+    }
 })
+
+const upload = multer({ storage: fileStorageEngine })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post('/single', upload.single('image'), (req, res) => {
+    console.log(req.file);
+    res.send("Single file is uploaded successfully")
+})
+
+
 app.listen(port, () => console.log(`server is running ${port}`))
